10fedfa9f41c65331def03150d3f0851c63d9ddc
PriyaaVishwakarma <priyavishwakarma11107@gmail.com>
2026-01-14 13:21:52 +0530

    foody

---
+++ app/src/App.jsx
@@ -0,0 +1,121 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import SearchResult from "./Components/SearchResult";
+
+export const BASE_URL = "http://localhost:9000";
+
+const App = () => {
+  const [data, setData] = useState(null);
+  const [filteredData,setFilteredData]=useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+
+  useEffect(() => {
+    const fetchFoodData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(BASE_URL);
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const json = await response.json();
+        setData(json);
+        setFilteredData(json);
+      } catch (error) {
+        setError("Unable to fetch data");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchFoodData(); 
+  }, []);
+
+  const searchFood =(e)=>{
+     const searchValue =e.target.value;
+
+     if (searchValue == ""){
+      setFilteredData(null);
+     }
+const filter = data?.filter((food)=>
+  food.name.tolowerCase().includes(searchValue.tolowerCase())
+
+);
+  setFilteredData(filter);
+  }
+
+ 
+
+
+  if (error) return <div>{error}</div>;
+  if (loading) return <div>Loading...</div>;
+
+  return (
+    <Container>
+      <TopContainer>
+        <div className="logo">
+          <img src="/Foody Zone.svg" alt="logo" />
+        </div>
+
+        <div className="search">
+          <input onChange={searchFood} placeholder="Search Food" />
+        </div>
+      </TopContainer>
+
+      <FilterContainer>
+        <Button>ALL</Button>
+        <Button>Breakfast</Button>
+        <Button>Lunch</Button>
+        <Button>Dinner</Button>
+      </FilterContainer>
+      <SearchResult data ={filteredData} />
+   
+    </Container>
+  );
+};
+
+export default App;
+
+ export const Container = styled.div`
+  background-color: #323343;
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+
+const TopContainer = styled.section`
+  min-height: 140px;
+  display: flex;
+  justify-content: space-between;
+  padding: 16px;
+  align-items: center;
+
+  .search {
+    input {
+      background-color: transparent;
+      border: 1px solid red;
+      color: white;
+      border-radius: 5px;
+      height: 40px;
+      font-size: 16px;
+      padding: 0 10px;
+    }
+  }
+`;
+
+const FilterContainer = styled.section`
+  display: flex;
+  justify-content: center;
+  gap: 12px;
+  padding-bottom: 40px;
+`;
+
+ export const Button = styled.button`
+  background: #ff4343;
+  border-radius: 5px;
+  padding: 6px 12px;
+  border: none;
+  color: white;
+  cursor: pointer;
+`;
+
